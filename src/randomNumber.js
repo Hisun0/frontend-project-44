35b3d90ca8b97d115ba43d1ee66c47b466dcad9c
Hisun0 <tridvad1@gmail.com>
2022-12-01 11:18:38 +0300

    added new random function

--- src/randomNumber.js
+++ src/randomNumber.js
@@ -6,4 +6,14 @@ function randomIndex() {
   return Math.round(Math.random() * 2);
 }
 
-export { randomIndex, randomNumber };
+function randomStep() {
+  return Math.round(Math.random() * 20);
+}
+
+function randomSpot() {
+  return Math.round(Math.random() * 9);
+}
+
+export {
+  randomIndex, randomNumber, randomStep, randomSpot,
+};
