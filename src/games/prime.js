356a91016c9ccd53882f58e3aa7ad843e194e446
Hisun0 <116166032+Hisun0@users.noreply.github.com>
2023-03-03 12:50:46 +0300

    Update prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,4 +1,4 @@
-import startGame from '../index.js';
+import startGame from '../engine.js';
 import { getRandomNumber } from '../tools.js';
 
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
