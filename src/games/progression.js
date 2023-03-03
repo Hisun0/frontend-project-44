68b9dc3d01be33ed38b25a0d06049f6b79672375
Hisun0 <116166032+Hisun0@users.noreply.github.com>
2023-03-03 12:50:59 +0300

    Update progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,4 +1,4 @@
-import startGame from '../index.js';
+import startGame from '../engine.js';
 import { getRandomNumber } from '../tools.js';
 
 const gameDescription = 'What number is missing in the progression?';
